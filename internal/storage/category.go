2007dc8fc11e3643688968ad8b0f6fc8fd26331f
vslitvinov <110770323+vslitvinov@users.noreply.github.com>
2022-11-28 23:31:06 +0500

    update

--- internal/storage/category.go
+++ internal/storage/category.go
@@ -1,2 +1,50 @@
 package storage
 
+import (
+	"context"
+	"sync"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/vslitvinov/catalogService/internal/models"
+	"github.com/vslitvinov/catalogService/internal/storage/psql"
+)
+
+
+type CategoryPSQLStorage interface {
+	Create(ctx context.Context, title, parentID string) (string, error) 
+	GetAll(ctx context.Context) ([]models.Category, error) 
+	FindByID(ctx context.Context, cid string) (models.Category, error)
+	FindByTitle(ctx context.Context, title string) (models.Category, error) 
+	Delete(ctx context.Context, cid string)  error 
+}
+
+type Category struct {
+	db CategoryPSQLStorage
+	cache sync.Map
+}
+
+
+func NewCategoryStorage(db *pgxpool.Pool) *Category{
+	c := Category{
+		db:psql.NewCategoryStorage(db),
+		cache: sync.Map{},
+	}
+
+	c.Start()
+
+	return &c
+}
+
+func (c *Category) Start (){
+	// load category db
+}
+
+
+func (c *Category) Create(ctx context.Context, title, parentID string) (string, error) {
+	
+}
+
+
+
+
+
---
+++ internal/storage/psql/product.go
@@ -0,0 +1,18 @@
+package psql
+
+import "github.com/jackc/pgx/v4/pgxpool"
+
+
+type Product struct {
+	pool  *pgxpool.Pool
+}
+
+// Create
+// Update
+// Delete
+// FindByID
+// FindByName
+// FindByShop
+// FindByCategory
+// FindByTags
+// FindByShopAndCategory
